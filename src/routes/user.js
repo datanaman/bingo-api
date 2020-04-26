a27ba37beed3dfcf777610c71f1326a5ca4496d2
Naman Jain <naman.jain@avanade.com>
2020-04-26 22:51:02 +0530

    added code

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get('/', (req, res) => {
   //return res.send('game selected data');
-  //return res.send(Object.values(req.context.models.users));
+  return res.send(Object.values(req.context.models.users));
 });
 
 router.get('/:userId', (req, res) => {
